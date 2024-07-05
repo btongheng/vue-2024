b975366de312313b5bb00a145534fc5ef66117a7
btongheng <102462410+btongheng@users.noreply.github.com>
2024-07-05 20:34:39 +0700

    add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,13 @@
+<template>
+  <Navbar>
+
+  </Navbar>
+</template>
+
 <script setup>
-import { onMounted, ref } from "vue";
-
-    const name = ref('John Doe');
-    const status = ref('active');
-    const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
-    const newTask = ref('');
-
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    };
-
-    const addTask = () => {
-      if (newTask.value.trim() !== '') {
-        tasks.value.push(newTask.value);
-        newTask.value = '';
-      }
-    }
-
-    const deletTask = (index) => {
-      tasks.value.splice(index, 1);
-    }
-
-    onMounted(async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-        const data = await response.json();
-        tasks.value = data.map(task => task.title);
-      } catch (error) {
-    }
-    })
+  import Navbar from './components/Navbar.vue';
 </script>
 
-<template>
-  <h1>Hello {{ name }}</h1>
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
+<style lang="scss" scoped>
 
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask" />
-    <button type="submit">Submit</button>
-  </form>
-  <h3>Task :</h3>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>{{ task }}</span>
-      <button @click="deletTask(index)">x</button>
-    </li>
-  </ul>
-  <br/>
-  <button @click="toggleStatus">Change Status</button>
-
-</template>
+</style>
